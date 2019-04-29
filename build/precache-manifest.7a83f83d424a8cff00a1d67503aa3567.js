3d4cad263ec6a2c2ccd55621ae39c4c9d6179bef
Guillermo <gdromo@hotmail.com>
2019-04-29 13:34:57 -0300

    1

---
+++ build/precache-manifest.7a83f83d424a8cff00a1d67503aa3567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e05a8a02a74bfb4d0af",
+    "url": "/webChancha/static/js/runtime~main.6e05a8a0.js"
+  },
+  {
+    "revision": "552ff88d4e2cc0b67b0b",
+    "url": "/webChancha/static/js/main.552ff88d.chunk.js"
+  },
+  {
+    "revision": "f650f9e5c4bd07e2df53",
+    "url": "/webChancha/static/js/1.f650f9e5.chunk.js"
+  },
+  {
+    "revision": "552ff88d4e2cc0b67b0b",
+    "url": "/webChancha/static/css/main.62f6c0dc.chunk.css"
+  },
+  {
+    "revision": "f650f9e5c4bd07e2df53",
+    "url": "/webChancha/static/css/1.a0615623.chunk.css"
+  },
+  {
+    "revision": "dd85a907e495d83f345ee2d018fc3b20",
+    "url": "/webChancha/index.html"
+  }
+];
